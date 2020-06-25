14477faafdb4073316943fdedb3dd6c686a46131
Gabriel Alves Silva <bielvipbh@hotmail.com>
2020-06-24 23:30:12 -0300

    Change icon

--- mobile/lib/splash_screen.dart
+++ mobile/lib/splash_screen.dart
@@ -4,7 +4,36 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Splash Screen')),
+      body: Container(
+        child: Stack(
+          fit: StackFit.expand,
+          children: <Widget>[
+            Image(
+              image: AssetImage(
+                'assets/splashscreen.png',
+              ),
+              fit: BoxFit.fill,
+              alignment: Alignment.bottomRight,
+            ),
+            Positioned(
+              bottom: 20,
+              left: 20,
+              child: SizedBox(
+                width: 20,
+                height: 20,
+                child: Center(
+                  child: CircularProgressIndicator(
+                    backgroundColor: Colors.white,
+                    valueColor: AlwaysStoppedAnimation<Color>(
+                        Color.fromRGBO(46, 46, 46, 1)),
+                    strokeWidth: 7,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
