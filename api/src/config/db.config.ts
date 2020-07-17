af5523b6d9f2369c87587ccfb5b9aea1ce130d85
Gabriel Alves Silva <bielvipbh@hotmail.com>
2020-07-17 17:33:42 -0300

    logging which env was used

--- api/src/config/db.config.ts
+++ api/src/config/db.config.ts
@@ -8,6 +8,8 @@ import { envFilePath } from './env.config';
 export const defaultConnectionConfig = async (): Promise<ConnectionOptions> => {
   dotenv.config({ path: envFilePath });
 
+  console.log(`env: ${process.env.NODE_ENV} file ${envFilePath}`);
+
   const connectionOptions = await getConnectionOptions();
 
   return Object.assign(connectionOptions, {
