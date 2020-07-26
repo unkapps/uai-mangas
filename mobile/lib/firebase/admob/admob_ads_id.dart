36946441ee6fec95a621f5a7f9f54fc5685d319b
Gabriel Alves Silva <bielvipbh@hotmail.com>
2020-07-26 16:42:30 -0300

    Change flutter_native_admob to admob_flutter
    Reason: performance

---
+++ mobile/lib/firebase/admob/admob_ads_id.dart
@@ -0,0 +1,5 @@
+class AdmobIdsId {
+  static final CHAPTER_PAGE = 'ca-app-pub-4719589372008331/1362451546';
+  static final LIST_GRIDVIEW = 'ca-app-pub-4719589372008331/4463257605';
+  static final MANGA_SINGLE_PAGE = 'ca-app-pub-4719589372008331/3952121711';
+}
