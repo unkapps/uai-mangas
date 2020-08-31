5e09f8d0aeefddcada09759d6032a17fdf983152
Gabriel Alves Silva <bielvipbh@hotmail.com>
2020-08-31 12:38:32 -0300

    On error use mangalivre

--- extrator/src/leitor-net-urls.ts
+++ extrator/src/leitor-net-urls.ts
@@ -1,59 +1,101 @@
 import { autoInjectable, singleton } from 'tsyringe';
 
 export const LEITOR_NET_URL = 'https://leitor.net';
-export const CATEGORIES_URL = `${LEITOR_NET_URL}/categories/categories_list.json`;
+export const MANGA_LIVRE_NET_URL = 'https://mangalivre.net';
 
-export const LEITOR_NET_DEFAULT_HTTP_HEADERS_ALL_ACCEPT = {
+const LEITOR_NET_DEFAULT_HTTP_HEADERS_ALL_ACCEPT = {
   authority: 'leitor.net',
   accept: '*/*',
-  'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36',
+  'user-agent':
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36',
   'Accept-Encoding': 'gzip',
 };
-export const LEITOR_NET_DEFAULT_HTTP_HEADERS = {
+
+const LEITOR_NET_DEFAULT_HTTP_HEADERS = {
   ...LEITOR_NET_DEFAULT_HTTP_HEADERS_ALL_ACCEPT,
   accept: 'application/json, text/javascript, */*; q=0.01',
 };
 
-export const LEITOR_NET_DEFAULT_HTTP_HEADERS_WITH_X_REQ = {
+const LEITOR_NET_DEFAULT_HTTP_HEADERS_WITH_X_REQ = {
   ...LEITOR_NET_DEFAULT_HTTP_HEADERS,
+  'x-requested-with': 'XMLHttpRequest',
+};
+
+const MANGA_LIVRE_DEFAULT_HTTP_HEADERS_ALL_ACCEPT = {
+  authority: 'mangalivre.net',
+  accept: '*/*',
+  'user-agent':
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36',
+  'Accept-Encoding': 'gzip',
+};
+
+const MANGA_LIVRE_DEFAULT_HTTP_HEADERS = {
+  ...MANGA_LIVRE_DEFAULT_HTTP_HEADERS_ALL_ACCEPT,
+  accept: 'application/json, text/javascript, */*; q=0.01',
+};
+
+const MANGA_LIVRE_DEFAULT_HTTP_HEADERS_WITH_X_REQ = {
+  ...MANGA_LIVRE_DEFAULT_HTTP_HEADERS,
   'x-requested-with': 'XMLHttpRequest',
 };
 
 @singleton()
 @autoInjectable()
 export default class LeitorNetUrls {
+  public useLeitorNet = true;
+
+  get url() {
+    return this.useLeitorNet ? LEITOR_NET_URL : MANGA_LIVRE_NET_URL;
+  }
+
+  get defaultHttpHeadersAllAccept() {
+    return this.useLeitorNet ? LEITOR_NET_DEFAULT_HTTP_HEADERS_ALL_ACCEPT : MANGA_LIVRE_DEFAULT_HTTP_HEADERS_ALL_ACCEPT;
+  }
+
+  get defaultHttpHeaders() {
+    return this.useLeitorNet ? LEITOR_NET_DEFAULT_HTTP_HEADERS : MANGA_LIVRE_DEFAULT_HTTP_HEADERS;
+  }
+
+  get defaultHttpHeadersWithXReq() {
+    return this.useLeitorNet ? LEITOR_NET_DEFAULT_HTTP_HEADERS_WITH_X_REQ : MANGA_LIVRE_DEFAULT_HTTP_HEADERS_WITH_X_REQ;
+  }
+
+  get categoriesUrl() {
+    return `${this.url}/categories/categories_list.json`;
+  }
+
   public getCategoriesUrl(): string {
-    return CATEGORIES_URL;
+    return this.categoriesUrl;
   }
 
   public getSeriesUrl(page: number, categoryId: number): string {
-    return `${LEITOR_NET_URL}/categories/series_list.json?page=${page}&id_category=${categoryId}`;
+    return `${this.url}/categories/series_list.json?page=${page}&id_category=${categoryId}`;
   }
 
   public getMangaUrl(mangaId: number, url?: string): string {
     if (url != null) {
-      return `${LEITOR_NET_URL}${url}`;
+      return `${this.url}${url}`;
     }
-    return `${LEITOR_NET_URL}/manga/a/${mangaId}`;
+    return `${this.url}/manga/a/${mangaId}`;
   }
 
   public getChaptersListUrl(page: number, mangaId: number): string {
-    return `${LEITOR_NET_URL}/series/chapters_list.json?page=${page}&id_serie=${mangaId}`;
+    return `${this.url}/series/chapters_list.json?page=${page}&id_serie=${mangaId}`;
   }
 
   public getImagesUrl(releaseId: number, token: string): string {
-    return `${LEITOR_NET_URL}/leitor/pages/${releaseId}.json?key=${token}`;
+    return `${this.url}/leitor/pages/${releaseId}.json?key=${token}`;
   }
 
   public newReleasesUrl(page: number): string {
-    return `${LEITOR_NET_URL}/home/releases?page=${page}`;
+    return `${this.url}/home/releases?page=${page}`;
   }
 
   public newMangasUrl(): string {
-    return `${LEITOR_NET_URL}/home/getNewSeries`;
+    return `${this.url}/home/getNewSeries`;
   }
 
   public sitemapUrl(): string {
-    return `${LEITOR_NET_URL}/leitor/sitemap/1.xml`;
+    return `${this.url}/leitor/sitemap/1.xml`;
   }
 }
--- extrator/src/service/page.service.ts
+++ extrator/src/service/page.service.ts
@@ -5,20 +5,14 @@ import Chapter from '../entity/chapter';
 import { ScanDto } from '../dto/chapter.dto';
 import generateUid from '../util/generator-id';
 import Page from '../entity/page';
-import LeitorNetUrls, {
-  LEITOR_NET_URL,
-  LEITOR_NET_DEFAULT_HTTP_HEADERS,
-  LEITOR_NET_DEFAULT_HTTP_HEADERS_WITH_X_REQ,
-} from '../leitor-net-urls';
+import LeitorNetUrls from '../leitor-net-urls';
 
 export const STATUS_FILE_PATH = './data/categories';
 
 @singleton()
 @autoInjectable()
 export default class PageService {
-  constructor(
-    private leitorNetUrls: LeitorNetUrls,
-  ) { }
+  constructor(private leitorNetUrls: LeitorNetUrls) {}
 
   public async createFromChapter(chapter: Chapter, scan: ScanDto): Promise<Page[]> {
     const token: string = await this.getTokenFromChapterPage(scan);
@@ -44,9 +38,9 @@ export default class PageService {
   }
 
   private async getTokenFromChapterPage(scan: ScanDto): Promise<string> {
-    const url: string = `${LEITOR_NET_URL}${scan.link}`;
+    const url: string = `${this.leitorNetUrls.url}${scan.link}`;
 
-    const headers = Object.assign(LEITOR_NET_DEFAULT_HTTP_HEADERS);
+    const headers = Object.assign(this.leitorNetUrls.defaultHttpHeaders);
     delete headers.accept;
 
     const response = await axios.get(url, {
@@ -62,7 +56,7 @@ export default class PageService {
   private async getImagesUrl(token: string, scan: ScanDto): Promise<string[]> {
     const url = this.leitorNetUrls.getImagesUrl(scan.id_release, token);
     const response = await axios.get(url, {
-      headers: LEITOR_NET_DEFAULT_HTTP_HEADERS_WITH_X_REQ,
+      headers: this.leitorNetUrls.defaultHttpHeadersWithXReq,
     });
 
     return response.data.images as string[];
